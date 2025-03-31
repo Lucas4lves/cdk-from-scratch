8fd5f83b5523537a9553f6d13af32733693f124f
lucas4lves <zombnfl@gmail.com>
2025-03-31 11:17:23 -0300

    Feat: dynamo table and naminghooks

--- src/infra/Launcher.ts
+++ src/infra/Launcher.ts
@@ -3,20 +3,24 @@ import { DataStack } from "./stacks/data/DataStack";
 import { LambdaStack } from "./stacks/lambda/LambdaStack";
 import { ProjectMetadata } from "../global/Global";
 import { GatewayStack } from "./stacks/gateway/GatewayStack";
+import { stackNamingHook, customResourceNamingHook } from "../global/NamingHook";
 
 const app = new App()
 const project_metadata : ProjectMetadata = {
     name: "cdk-basics",
-    maintainers: ["Lucas Alves"]
+    maintainers: ["Lucas Alves"],
+    squad: "devops"
 }
 
-new DataStack(app, 'DataStack')
-const lambdaStack = new LambdaStack(app, 'LambdaStack', {
+new DataStack(app, stackNamingHook(project_metadata,"data"), {
+    metadata : project_metadata,
+})
+const lambdaStack = new LambdaStack(app, stackNamingHook(project_metadata,"lambda"), {
     stackSlug: "lambda-stack",
     functionSlug: "",
     metadata: project_metadata
 })
 
-new GatewayStack(app,'GatewayStack', {
+new GatewayStack(app,stackNamingHook(project_metadata,"rest-api"), {
     helloLambdaIntegration: lambdaStack.helloIntegration
 })
